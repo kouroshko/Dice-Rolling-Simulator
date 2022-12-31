3ec07a5db1cd988ffedad04db425e5be59791ea9
kouroshko <91155769+kouroshko@users.noreply.github.com>
2022-12-31 15:28:52 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,21 @@
+import random
+number = random.randint(1, 6)
+
+
+if number == 1:
+    print("|0|  1")
+
+if number == 2:
+    print("|00|  2")
+
+if number == 3:
+    print("|000|  3")
+
+if number == 4:
+    print("|0000| 4")
+
+if number == 5:
+    print("|00000|  5")
+
+if number == 6:
+    print("|000000|  6")
